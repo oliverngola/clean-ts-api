9f40a9175395357c32f2b59af3b30f76e76f46bb
Oliveira Ngola <oliveiraernestongola123@gmail.com>
2020-11-06 11:46:31 +0100

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts'
+  ],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  tranform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
