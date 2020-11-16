b8d1ddd739bac7bb2bd1621f9925501e3fc19c61
Oliveira Ngola <oliveiraernestongola123@gmail.com>
2020-11-16 22:15:35 +0100

    feat: ensure AccountMongoRepository returns an account on success

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb'
+import { Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
   client: null as MongoClient,
@@ -9,7 +9,12 @@ export const MongoHelper = {
       useUnifiedTopology: true
     })
   },
+
   async disconnect () {
     await this.client.close()
+  },
+
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
   }
 }
