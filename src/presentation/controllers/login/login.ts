b77f4ecda63b278c7285d92796f28809013e8d62
Oliveira Ngola <oliveiraernestongola123@gmail.com>
2020-12-15 17:09:05 +0100

    refactor: remove code duplication

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -17,7 +17,8 @@ export class LoginControler implements Controller {
         return badRequest(new MissingParamError(field))
       }
     }
-    const isValid = this.emailValidator.isValid(httpRequest.body.email)
+    const { email } = httpRequest.body
+    const isValid = this.emailValidator.isValid(email)
     if (!isValid) {
       return badRequest(new InvalidParamError('email'))
     }
