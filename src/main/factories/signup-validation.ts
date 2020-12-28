2450da29b8a83306c4c9bb13ce25433a5fd22d24
Oliveira Ngola <oliveiraernestongola123@gmail.com>
2020-12-28 14:51:52 +0100

    refactor: move EmailValidation to the ValidationComposite

--- src/main/factories/signup-validation.ts
+++ src/main/factories/signup-validation.ts
@@ -1,7 +1,9 @@
 import { CompareFieldValidation } from '../../presentation/helpers/validators/compare-field-validation'
+import { EmailValidation } from '../../presentation/helpers/validators/email-validation'
 import { RequiredFieldValidation } from '../../presentation/helpers/validators/required-field-validation'
 import { Validation } from '../../presentation/helpers/validators/validation'
 import { ValidateComposite } from '../../presentation/helpers/validators/validation-composite'
+import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 
 export const makeSignUpValidation = (): ValidateComposite => {
   const validations: Validation[] = []
@@ -9,5 +11,6 @@ export const makeSignUpValidation = (): ValidateComposite => {
     validations.push(new RequiredFieldValidation(field))
   }
   validations.push(new CompareFieldValidation('password', 'passwordConfirmation'))
+  validations.push(new EmailValidation('email', new EmailValidatorAdapter()))
   return new ValidateComposite(validations)
 }
