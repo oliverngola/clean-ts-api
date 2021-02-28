b8c89ea8c2d9ff7cedb082bd6253441b66d3071e
Oliveira Ngola <oliveiraernestongola123@gmail.com>
2021-02-28 14:03:27 +0100

    test: ensure AddSurveyController returns 204 on success

--- src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
@@ -1,6 +1,6 @@
 import { HttpRequest, Validation, AddSurvey, AddSurveyModel } from './add-survey-controller-protocols'
 import { AddSurveyController } from './add-survey-controller'
-import { badRequest, serverError } from '../../../helpers/http/http-helpers'
+import { badRequest, notContent, serverError } from '../../../helpers/http/http-helpers'
 import { MissingParamError } from '../../../errors'
 
 const makeValidation = (): Validation => {
@@ -73,5 +73,11 @@ test('AddSurvey Controller', () => {
     jest.spyOn(addSurveyStub, 'add').mockResolvedValueOnce(new Promise((resolve, reject) => reject(new Error())))
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(serverError(new Error()))
+  })
+
+  describe('Should return 204 on success', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(notContent())
   })
 })
