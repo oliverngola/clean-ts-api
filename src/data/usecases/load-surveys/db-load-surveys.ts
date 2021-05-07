e0636313598e29806d3ee14184088f0d526aecd3
Oliveira Ngola <oliveiraernestongola123@gmail.com>
2021-05-07 22:05:30 +0100

    feat: ensure DbLoadSurveys calls LoadSurveysRepository

---
+++ src/data/usecases/load-surveys/db-load-surveys.ts
@@ -0,0 +1,16 @@
+import {
+  SurveyModel,
+  LoadSurveys,
+  LoadSurveysRepository
+} from './db-load-surveys-protocols'
+
+export class DbLoadSurveys implements LoadSurveys {
+  constructor (
+    private readonly loadSurveysRepository: LoadSurveysRepository
+  ) {}
+
+  async load (): Promise<SurveyModel[]> {
+    await this.loadSurveysRepository.loadAll()
+    return null
+  }
+}
