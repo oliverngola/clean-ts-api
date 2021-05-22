78c9c218d456fddbd6c68070c13a3d45cb7c6fc7
Oliveira Ngola <oliveiraernestongola123@gmail.com>
2021-05-22 22:00:11 +0100

    docs: add login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,32 @@
+import { loginPath } from './paths'
+import {
+  accountSchema,
+  loginParamsSchema
+} from './schemas'
+
 export default {
   openapi: '3.0.0',
   info: {
     title: 'Clean Node API',
     description: 'API para realizar enquetes entre programadores',
     version: '1.0.1'
+  },
+  servers: [{
+    url: '/api',
+    description: 'Development server'
+  },
+  {
+    url: ' https://clean-node-api-ongola.herokuapp.com/api',
+    description: 'Production server'
+  }],
+  tags: [{
+    name: 'Login'
+  }],
+  paths: {
+    '/login': loginPath
+  },
+  schemas: {
+    account: accountSchema,
+    loginParams: loginParamsSchema
   }
 }
