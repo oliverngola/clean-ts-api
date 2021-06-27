2229c85e5ac34ecf1412fadea1353ee91ce9cb35
Oliveira Ngola <oliveiraernestongola123@gmail.com>
2021-06-27 23:46:40 +0100

    refactor: use resolve instead of template literals to sync routes files

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,10 +1,11 @@
 import { readdirSync } from 'fs'
+import { resolve } from 'path'
 import { Express, Router } from 'express'
 
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
-  readdirSync(`${__dirname}/../routes`).map(async file => {
+  readdirSync(resolve(__dirname,'../routes')).map(async file => {
     if (!file.includes('.test.') && !file.endsWith('.map')) {
       (await import(`../routes/${file}`)).default(router)
     }
