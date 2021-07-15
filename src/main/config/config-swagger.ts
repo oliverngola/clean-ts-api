5035e7e1471a80a173bb6cda2e7b8602877cb158
Oliveira Ngola <oliveiraernestongola123@gmail.com>
2021-07-15 01:26:53 +0100

    refactor: make default route redirect to api docs

--- src/main/config/config-swagger.ts
+++ src/main/config/config-swagger.ts
@@ -1,8 +1,11 @@
 import swaggerConfig from '@/main/docs'
 import { noCache } from '@/main/middlewares'
-import { Express } from 'express'
+import { Express, Request, Response } from 'express'
 import { serve, setup } from 'swagger-ui-express'
 
 export default (app: Express): void => {
+  app.get('/',(request: Request, response: Response) => {
+    response.redirect('/api-docs')
+  })
   app.use('/api-docs',noCache, serve, setup(swaggerConfig))
 }
