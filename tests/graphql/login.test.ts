7e14b9f62849ab35a9f946f2e23a01bf7e57e69f
Oliveira Ngola <oliveiraernestongola123@gmail.com>
2021-08-12 16:21:19 +0100

    test: ensure SignUp Mutation  returns an Account on valid date

--- tests/graphql/login.test.ts
+++ tests/graphql/login.test.ts
@@ -64,4 +64,29 @@ describe('Login GraphQL', () => {
       expect(res.errors[0].message).toBe('Unauthorized')
     })
   })
+
+  describe('SignUp Mutation', () => {
+    const signUpMutation = gql`
+      mutation signUp($name: String!, $email: String!, $password: String!, $passwordConfirmation: String!) {
+        signUp(name: $name, email: $email, password: $password, passwordConfirmation: $passwordConfirmation ) {
+          accessToken,
+          name
+        }
+      }
+    `
+
+    test('Should return an Account on valid date', async () => {
+      const { mutate } = createTestClient({ apolloServer })
+      const res: any = await mutate(signUpMutation, {
+        variables: {
+          name: 'Oliver',
+          email: 'oliveiraernestongola123@gmail.com',
+          password: '123',
+          passwordConfirmation: '123'
+        }
+      })
+      expect(res.data.signUp.accessToken).toBeTruthy()
+      expect(res.data.signUp.name).toBe('Oliver')
+    })
+  })
 })
